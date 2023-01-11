8eb96be020e4449aaf49b06070e94872f6dba14d
VicenteVicente <vicente.calisto98@gmail.com>
2023-01-11 15:33:15 -0300

    query method

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from pymdb import Client
 
 if __name__ == "__main__":
     conn = Client("localhost", 8080)
-    conn.query("MATCH (n) RETURN n LIMIT 10")
+    conn.query("MATCH (?n) RETURN ?n LIMIT 10")
--- pymdb.py
+++ pymdb.py
@@ -1,14 +1,32 @@
 import socket
 
+BYTES_FOR_QUERY_LENGTH = 4
+BUFFER_SIZE = 4096
+END_MASK = 0x80
+STATUS_MASK = 0x7F
+
 class Client:
     def __init__(self, host: str, port: int):
         self.addr = (host, port)
         self.sock = socket.create_connection(self.addr)
 
-    def query(self, query: str):
-        query_b = query.encode("utf-8")
-        size_b  = len(query_b).to_bytes(4, byteorder="little")
-        self.sock.send(size_b + query_b)
+    def query(self, query: str) -> int:
+        # Send the query to the server
+        query_bytes = query.encode("utf-8")
+        size_bytes = len(query_bytes).to_bytes(
+            BYTES_FOR_QUERY_LENGTH, byteorder="little"
+        )
+        self.sock.send(size_bytes + query_bytes)
+        # Receive the response from the server
+        result_buffer = bytearray(BUFFER_SIZE)
+        while True:
+            result_buffer = self.sock.recv(BUFFER_SIZE)
+            reply_length = int.from_bytes(result_buffer[1:3], byteorder="little")
+            # First 3 bytes are used for the status and tlength of the message
+            print(result_buffer[3:reply_length - 3].decode("utf-8"))
+            if (result_buffer[0] & END_MASK) != 0:
+                break
+        return result_buffer[0] & STATUS_MASK
 
     def close(self):
         self.sock.shutdown(socket.SHUT_RDWR)
