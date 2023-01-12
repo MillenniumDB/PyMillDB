47244b0c87b2c81d6723862e90011563976823fb
VicenteVicente <vicente.calisto98@gmail.com>
2023-01-12 11:58:38 -0300

    feat: output as dataframe

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from pymdb import Client
+import pandas as pd
 
 if __name__ == "__main__":
+    # 1. Connect to MDB
     conn = Client("localhost", 8080)
-    conn.query("MATCH (?n) RETURN ?n LIMIT 10")
+    # 2. Query MDB
+    try:
+        result = conn.query("MATCH (?x) RETURN ?x LIMIT 100")
+        df = result.to_dataframe()
+        print(df)
+    except Exception as e:
+        print("Query error: ", e)
+    conn.close()
--- pymdb.py
+++ pymdb.py
@@ -1,9 +1,22 @@
+import pandas as pd
 import socket
+import tempfile
 
 BYTES_FOR_QUERY_LENGTH = 4
 BUFFER_SIZE = 4096
 END_MASK = 0x80
 STATUS_MASK = 0x7F
+
+class Result:
+    def __init__(self):
+        self.tmp_file = tempfile.TemporaryFile()
+
+    def append(self, chunk: bytes):
+        self.tmp_file.write(chunk)
+
+    def to_dataframe(self):
+        self.tmp_file.seek(0)
+        return pd.read_csv(self.tmp_file)
 
 class Client:
     def __init__(self, host: str, port: int):
@@ -19,14 +32,18 @@ class Client:
         self.sock.send(size_bytes + query_bytes)
         # Receive the response from the server
         result_buffer = bytearray(BUFFER_SIZE)
+        result = Result()
         while True:
             result_buffer = self.sock.recv(BUFFER_SIZE)
             reply_length = int.from_bytes(result_buffer[1:3], byteorder="little")
             # First 3 bytes are used for the status and tlength of the message
-            print(result_buffer[3:reply_length - 3].decode("utf-8"))
+            result.append(result_buffer[3:reply_length - 3])
             if (result_buffer[0] & END_MASK) != 0:
                 break
-        return result_buffer[0] & STATUS_MASK
+        status = result_buffer[0] & STATUS_MASK
+        if status != 0:
+            raise Exception(f"Query failed with status code {status}")
+        return result
 
     def close(self):
         self.sock.shutdown(socket.SHUT_RDWR)
