a1597434540b35d4a265fe0fedfcc249140b94fe
VicenteVicente <vicente.calisto98@gmail.com>
2023-02-21 15:15:14 -0300

    cleaned repo

--- other/torch_geometric/mdb_neighbor_loader.py
+++
@@ -1,6 +0,0 @@
-from torch.utils.data import DataLoader
-
-
-class MDBNeighborLoader(DataLoader):
-    def __init__(self, data):
-        pass
--- store.py
+++
@@ -1,6 +0,0 @@
-from pymdb import FeatureStore, MDBClient
-
-with MDBClient(host="127.0.0.1", port=8080) as client:
-    fs = FeatureStore(client, "random")
-    print(fs)
-    fs.close()
