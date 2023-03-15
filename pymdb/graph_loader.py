a4f1301dca4f79d688d99c87a3a430cc35eba165
VicenteVicente <vicente.calisto98@gmail.com>
2023-03-15 10:51:34 -0300

    refactor(graph): moved to graph_loader module

--- pymdb/graph_loader.py
+++ pymdb/graph_loader.py
@@ -6,12 +6,40 @@ from typing import TYPE_CHECKING, List
 
 import torch
 
-from .graph import Graph
 from .protocol import RequestType, StatusCode
 from .utils import decorators, packer
 
 if TYPE_CHECKING:
     from .mdb_client import MDBClient
+
+
+class Graph:
+    def __init__(
+        self,
+        node_features: torch.Tensor,  # [num_nodes, feature_size]
+        node_labels: torch.Tensor,  # [num_nodes]
+        edge_index: torch.Tensor,  # [2, num_edges]
+        node_ids: List[int],  # [num_nodes]
+        num_seeds: int,
+        feature_size: int,
+    ) -> None:
+        self.node_features = node_features
+        self.node_labels = node_labels
+        self.edge_index = edge_index
+        self.node_ids = node_ids
+        self.num_seeds = num_seeds
+        self.feature_size = feature_size
+
+    def __repr__(self) -> str:
+        return (
+            "Graph("
+            + f"node_features={list(self.node_features.size())}, "
+            + f"node_labels={list(self.node_labels.size())}, "
+            + f"edge_index={list(self.edge_index.size())}, "
+            + f"node_ids=[{len(self.node_ids)}], "
+            + f"num_seeds={self.num_seeds}, "
+            + f"feature_size={self.feature_size})"
+        )
 
 
 class GraphLoader(abc.ABC):
