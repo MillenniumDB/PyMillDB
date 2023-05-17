b52c1b96f52922c4b11e43bb3455d3c722ae6866
VicenteVicente <vicente.calisto98@gmail.com>
2023-05-17 10:59:23 -0400

    removed train graph loader

--- pymdb/__init__.py
+++ pymdb/__init__.py
@@ -1,11 +1,10 @@
-from .graph_loader import EvalGraphLoader, SamplingGraphLoader, TrainGraphLoader
+from .graph_loader import EvalGraphLoader, SamplingGraphLoader
 from .mdb_client import MDBClient
 from .sampler import Sampler
 from .tensor_store import TensorStore
 
 __all__ = [
     "EvalGraphLoader",
-    "TrainGraphLoader",
     "SamplingGraphLoader",
     "MDBClient",
     "Sampler",
